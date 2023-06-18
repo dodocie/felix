ef60a37bb0ebfbfd12add5de817a3729b50d7a1b
Kiera <65538313@qq.com>
2023-06-18 13:16:20 +0800

    fix: touch-err-prone in home page
    The menu wrapped in built-in Link in block style was touch-err-prone on cell phone

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Social from '@/components/Social'
 import Search from '@/components/Search'
 import ReadOnBanner from '@/components/ReadOn'
 import Paw from '@/icons/Paw'
+import { spawn } from 'child_process'
 
 export default function Home() {
-
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -23,30 +23,26 @@ export default function Home() {
       </div>
 
       <div className={styles.grid}>
-        <Link href="/blog"
-          className={styles.card}
-        >
-          <h2>
+        <div className='mb-8'>
+          <Link href='/blog' className={`font-semibold ${styles.card}`}>
             前端探索 <span>-&gt;</span>
-          </h2>
-          <p>にゃ～(^._.^)ﾉ代码可不能比喵粑粑还臭</p>
-        </Link>
-        <Link href="/essay"
-          className={styles.card}
-        >
-          <h2>
+          </Link>
+          <p className='mt-2 text-base text-gray-500'>
+            にゃ～(^._.^)ﾉ代码可不能比喵粑粑还臭
+          </p>
+        </div>
+        <div className='mb-8'>
+          <Link href='/essay' className={`font-semibold ${styles.card}`}>
             灵感拾遗 <span>-&gt;</span>
-          </h2>
-          <p>一些碎片化的知识和趣闻。</p>
-        </Link>
-        <Link href="/"
-          className={styles.card}
-        >
-          <h2>
+          </Link>
+          <p className='mt-2 text-base text-gray-500'>一些碎片化的知识和趣闻。</p>
+        </div>
+        <div className='mb-8'>
+          <Link href='/' className={`font-semibold ${styles.card}`}>
             AI 自习室 <span>-&gt;</span>
-          </h2>
-          <p>还没想好怎么做~</p>
-        </Link>
+          </Link>
+          <p className='mt-2 text-base text-gray-500'>还没想好怎么做~</p>
+        </div>
       </div>
     </main>
   )
